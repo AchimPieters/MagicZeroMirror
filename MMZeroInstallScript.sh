f5acdf847609ddc36e0c8aaaa9cb39ceec047b9a
Achim Pieters <info@studiopieters.nl>
2020-01-03 11:07:34 +0100

    Version 3.2.6

--- MMZeroInstallScript.sh
+++ MMZeroInstallScript.sh
@@ -64,9 +64,9 @@ sudo plymouth-set-default-theme -R MagicMirror;
 
 echo 'Copy Magic Mirror2 startup scripts'
 cd ~;
-sudo mv Raspberry-MagicMirror/mmstart.sh home/pi/mmstart.sh;
-sudo mv Raspberry-MagicMirror/chromium_start.sh home/pi/chromium_start.sh;
-sudo mv Raspberry-MagicMirror/pm2_MagicMirror.json home/pi/pm2_MagicMirror.json;
+sudo mv ~/Raspberry-MagicMirror/mmstart.sh ~/home/pi/mmstart.sh;
+sudo mv ~/Raspberry-MagicMirror/chromium_start.sh ~/home/pi/chromium_start.sh;
+sudo mv ~/Raspberry-MagicMirror/pm2_MagicMirror.json ~/home/pi/pm2_MagicMirror.json;
 sudo chmod a+x mmstart.sh;
 sudo chmod a+x chromium_start.sh;
 sudo chmod a+x pm2_MagicMirror.json;
