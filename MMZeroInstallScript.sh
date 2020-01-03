227dd42621af525e15689a063ba6844df6e490d7
Achim Pieters <achimpieters@hotmail.com>
2020-01-03 14:30:17 +0100

    Update MMZeroInstallScript.sh

--- MMZeroInstallScript.sh
+++ MMZeroInstallScript.sh
@@ -74,6 +74,6 @@ sudo chmod a+x pm2_MagicMirror.json;
 echo 'Use pm2 control like a service MagicMirror'
 sudo npm install -g pm2;
 sudo env PATH=$PATH:/usr/local/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
-pm2 start ~/home/pi/pm2_MagicMirror.json;
+pm2 start ~/pm2_MagicMirror.json;
 pm2 save;
 echo 'Magic Mirror should begin shortly'
