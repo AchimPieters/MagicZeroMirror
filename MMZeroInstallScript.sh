3143087ed7d774dd7c0e818ba75cc938a0ed5ff6
Achim Pieters <achimpieters@hotmail.com>
2025-03-04 12:56:07 +0100

    Update MMZeroInstallScript.sh

--- MMZeroInstallScript.sh
+++ MMZeroInstallScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2020 Achim Pieters | StudioPieters®
+# Copyright 2025 Achim Pieters | StudioPieters®
 #
 # More information on https://Studiopieters.nl
 #
@@ -16,70 +16,58 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NO NINFRINGEMENT. IN NO EVENT SHALL THE
+# FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+
+set -e  # Exit on any error
+
 echo 'Downloading MagicMirror Raspberry Pi Zero W installation files'
-git clone --recursive https://github.com/AchimPieters/MagicZeroMirror.git
+git clone --recursive https://github.com/AchimPieters/MagicZeroMirror.git || { echo "Failed to clone repository"; exit 1; }
 
-echo 'Updating Pi'
-sudo apt-get update;
+echo 'Updating Raspberry Pi...'
+sudo apt-get update -y && sudo apt-get upgrade -y --fix-missing
 
-echo 'Upgrading Pi'
-sudo apt-get upgrade;
-sudo apt-get upgrade --fix-missing;
+echo 'Installing dependencies...'
+sudo apt install -y npm git xinit xorg matchbox unclutter chromium-browser
 
-echo 'Downloading node v10.16.0'
-sudo wget https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-armv6l.tar.xz;
+echo 'Downloading and installing Node.js (v18)'
+NODE_VERSION="v18.19.0"
+NODE_ARCH="linux-armv6l"
+NODE_TAR="node-${NODE_VERSION}-${NODE_ARCH}.tar.xz"
 
-echo 'Extracting node v11.6.0'
-tar xvf node-v10.16.0-linux-armv6l.tar.xz;
-cd node-v10.16.0-linux-armv6l;
-sudo cp -R * /usr/local/;
+wget -q --show-progress "https://nodejs.org/dist/${NODE_VERSION}/${NODE_TAR}"
+tar xf ${NODE_TAR}
+sudo cp -R node-${NODE_VERSION}-${NODE_ARCH}/* /usr/local/
+rm -rf node-${NODE_VERSION}-${NODE_ARCH} ${NODE_TAR}
 
-echo 'Installing Magic Mirror Dependencies'
-cd ~;
-sudo apt install npm -y;
-sudo apt install git;
+echo 'Cloning the latest version of MagicMirror...'
+git clone https://github.com/MichMich/MagicMirror.git
 
-echo 'Cloning the latest version of Magic Mirror2'
-git clone https://github.com/MichMich/MagicMirror;
+echo 'Installing MagicMirror dependencies...'
+cd MagicMirror
+npm install --arch=armv7l
 
-echo 'Installing Magic Mirror Dependencies'
-cd MagicMirror;
-npm install --arch=armv7l;
-sudo apt install chromium-browser -y;
-sudo apt-get install xinit -y;
-sudo apt install xorg -y;
-sudo apt install matchbox -y;
-sudo apt install unclutter -y;
+echo 'Loading default config...'
+cp config/config.js.sample config/config.js
 
-echo 'Loading default config'
-cp config/config.js.sample config/config.js;
+echo 'Setting MagicMirror splash screen...'
+THEME_DIR="/usr/share/plymouth/themes/MagicMirror"
+sudo mkdir -p ${THEME_DIR}
+sudo cp ~/MagicMirror/splashscreen/{splash.png,MagicMirror.plymouth,MagicMirror.script} ${THEME_DIR}/
+sudo plymouth-set-default-theme -R MagicMirror
 
-echo 'Set the splash screen to be magic mirror'
-THEME_DIR="/usr/share/plymouth/themes"
-sudo mkdir $THEME_DIR/MagicMirror;
-sudo cp ~/MagicMirror/splashscreen/splash.png $THEME_DIR/MagicMirror/splash.png && sudo cp ~/MagicMirror/splashscreen/MagicMirror.plymouth $THEME_DIR/MagicMirror/MagicMirror.plymouth && sudo cp ~/MagicMirror/splashscreen/MagicMirror.script $THEME_DIR/MagicMirror/MagicMirror.script;
-sudo plymouth-set-default-theme -R MagicMirror;
+echo 'Copying MagicMirror startup scripts...'
+cd ~
+sudo mv ~/MagicZeroMirror/{mmstart.sh,chromium_start.sh,pm2_MagicMirror.json} ~/
+sudo chmod +x ~/mmstart.sh ~/chromium_start.sh ~/pm2_MagicMirror.json
 
-echo 'Copy Magic Mirror2 startup scripts'
-cd ~;
-sudo mv ~/MagicZeroMirror/mmstart.sh ~/mmstart.sh;
-sudo mv ~/MagicZeroMirror/chromium_start.sh ~/chromium_start.sh;
-sudo mv ~/MagicZeroMirror/pm2_MagicMirror.json ~/pm2_MagicMirror.json;
+echo 'Installing and setting up PM2 for auto-start...'
+sudo npm install -g pm2
+pm2 startup systemd -u pi --hp /home/pi
+pm2 start ~/pm2_MagicMirror.json
+pm2 save
 
-sudo chmod a+x mmstart.sh;
-sudo chmod a+x chromium_start.sh;
-sudo chmod a+x pm2_MagicMirror.json;
-
-echo 'Use pm2 control like a service MagicMirror'
-cd ~;
-sudo npm install -g pm2;
-pm2 startup;
-sudo env PATH=$PATH:/usr/local/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi;
-pm2 start pm2_MagicMirror.json;
-pm2 save;
-echo 'Magic Mirror should begin shortly'
+echo 'Installation complete. MagicMirror should start shortly!'
